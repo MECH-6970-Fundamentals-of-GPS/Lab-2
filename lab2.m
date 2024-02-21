ca1db9af9a2c180bbc704eb2c4b5f3c855e46ce0
Walter Livingston <wbl0009@auburn.edu>
2024-02-21 16:26:21 -0600

    code cleanup

--- lab2.m
+++ lab2.m
@@ -1,37 +1,40 @@
 %% PART I
 clear; close all; clc;
 
-addpath(genpath("./"));
+% Constants
+c = physconst('LightSpeed');
+fL1 = 1575.42e6;
+filename = 'RCVR_S1_data.mat';
+toomersLLA = [32.606389, -85.481667, 214.65];
 
-data = load("RCVR_S1_data.mat").RCVR_S1;
+% Load Data
+addpath(genpath("./"));
+data = load(filename).RCVR_S1;
 ephem = data.ephem;
 psrL1 = data.measurements.L1.psr;
 time = data.GPS_time.seconds;
-c=physconst('LightSpeed');
-fL1=1575.42e6;
 
 for i = 1:length(time)
     % Part A: Calculate the sv pos as a function of GPS time
     [svPos, svVel, svB, svD, svPrns] = sv_positions(ephem, psrL1(i,:), time(i));
 
     % Part B: Calculate User Position
-    psr=(data.measurements.L1.psr(i,svPrns)+c*svB')';
-    doppler=data.measurements.L1.doppler(i,svPrns);
-    psrR=((-c.*doppler./fL1)+c*svD')';
-    [xhat(i,:),DOP(:,:,i)]=gpsPosVel(psr,[0,0,0,0],svPos,psrR,svVel, eye(length(svPrns)));
+    psr = (data.measurements.L1.psr(i, svPrns) + c*svB')';
+    doppler = data.measurements.L1.doppler(i, svPrns);
+    psrR = ((-c.*doppler./fL1)+c*svD')';
+    [Xu(i,:), DOP(:,:,i)] = gpsPosVel(psr,[0,0,0,0],svPos,psrR,svVel, eye(length(svPrns)));
 end
 
 % Part C: Convert ECEF to LLA and Plot
-uPosLLA=ecef2lla(xhat(:,1:3));
+uPosLLA = ecef2lla(Xu(:,1:3));
 figure()
 geoplot(uPosLLA(:,1),uPosLLA(:,2),'.')
 geobasemap satellite
 
 % Part D: Convert ECEF to ENU
-time=time-time(1);
-ToomersLLA=[32.606389,-85.481667,214.65];
-wgs=wgs84Ellipsoid("meter");
-[uPosE,uPosN,uPosU]=ecef2enu(xhat(:,1),xhat(:,2),xhat(:,3),ToomersLLA(1),ToomersLLA(2),ToomersLLA(3),wgs);
+time = time - time(1);
+wgs = wgs84Ellipsoid("meter");
+[uPosE,uPosN,uPosU] = ecef2enu(Xu(:,1), Xu(:,2), Xu(:,3), toomersLLA(1), toomersLLA(2), toomersLLA(3),wgs);
 figure()
 plot(time,uPosE)
 title('East','.')
@@ -42,10 +45,9 @@ figure()
 plot(time,uPosU)
 title('Up','.')
 
-
 % Part E: Calculate and find User Velocity in ENU
 % Note: ECEF Velocity states computed in gpsPosVel function
-[uVelE,uVelN,uVelU]=ecef2enu(xhat(:,4),xhat(:,5),xhat(:,6),ToomersLLA(1),ToomersLLA(2),ToomersLLA(3),wgs);
+[uVelE,uVelN,uVelU] = ecef2enu(Xu(:,4),Xu(:,5),Xu(:,6),toomersLLA(1),toomersLLA(2),toomersLLA(3),wgs);
 
 
 
