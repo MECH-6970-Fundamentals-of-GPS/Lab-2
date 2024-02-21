41fe0e8b784ff7551dadb03f46f6c311b8ae39e4
Walter Livingston <wblivingston18@gmail.com>
2024-02-21 17:46:43 -0600

    cleanup 2

--- gpsPosVel2.m
+++ gpsPosVel2.m
@@ -1,4 +1,4 @@
-function [Xs, DOP] = gpsPosVel(psr, uPos, sPos, psrR, sVel, R)
+function [Xs, DOP] = gpsPosVel2(psr, X0, sPos, psrR, sVel, R)
 % Performs Weighted Least Squares on GPS pseudoranges and range rates
 %   Given satellite positions, velocites, and pseudoranges/range rates this 
 %   function calculates the receiver position and velocity in the ECEF 
@@ -18,16 +18,16 @@ function [Xs, DOP] = gpsPosVel(psr, uPos, sPos, psrR, sVel, R)
 %    UserStates : The estimated user state [x,y,z,vx,vy,vz,b,d] 
 %    DOP        : Dilution of Precision Matrix [8,8]
 
-dX = 1e3 * ones(length(uPos),1);
+dX = 1e3 * ones(length(X0),1);
 for i=1:100
-    dr = sPos - uPos(i,1:3);                % True Range
+    dr = sPos - X0(i,1:3);                % True Range
     r = vecnorm(dr, 2, 2);                  % Range Unit Vectors
     U = dr ./ r;                            % LOS Vector
     H = [-U, ones(length(psr),1)];          % Geometry Matrix
-    rho_hat = r + uPos(i,4);                % Estimated psr
+    rho_hat = r + X0(i,4);                % Estimated psr
     delpsr = psr - rho_hat;                 % Meas-Est Residual
     dX = (H'*R*H)\H'*R*delpsr;             % Least Squares
-    uPos(i+1,:)=uPos(i,:)+dX';             % Newton-Raphson Update
+    X0(i+1,:)=X0(i,:)+dX';             % Newton-Raphson Update
     if (i == 100)
         disp('Solution did not converge\n')
         break
@@ -40,9 +40,9 @@ rrTilde=psrR-dot(sVel,U,2);                                     % Range Rate Mea
 vel=(H'*H)\H'*rrTilde;                                          % Least Squares
 
 Xs=zeros(1,8);
-Xs(1,1:3) = uPos(end,1:3);
+Xs(1,1:3) = X0(end,1:3);
 Xs(1,4:6) = vel(1:3);
-Xs(1,7) = uPos(end,4);
+Xs(1,7) = X0(end,4);
 Xs(1,8) = vel(4);
 DOP=inv(H'*R*H);
 end
--- lab2.m
+++ lab2.m
@@ -12,17 +12,20 @@ addpath(genpath("./"));
 data = load(filename).RCVR_S1;
 ephem = data.ephem;
 psrL1 = data.measurements.L1.psr;
+dopL1 = data.measurements.L1.doppler;
+psrL2 = data.measurements.L2.psr;
 time = data.GPS_time.seconds;
+
+X0 = [0 0 0 0];
 
 for i = 1:length(time)
     % Part A: Calculate the sv pos as a function of GPS time
     [svPos, svVel, svB, svD, svPrns] = sv_positions(ephem, psrL1(i,:), time(i));
 
     % Part B: Calculate User Position
-    psr = (data.measurements.L1.psr(i, svPrns) + c*svB')';
-    doppler = data.measurements.L1.doppler(i, svPrns);
-    psrR = ((-c.*doppler./fL1)+c*svD')';
-    [Xu(i,:), DOP(:,:,i)] = gpsPosVel(psr,[0,0,0,0],svPos,psrR,svVel, eye(length(svPrns)));
+    rho = psrL1(i, svPrns)' + c*svB;
+    rho_dot = -c.*dopL1(i, svPrns)'./fL1 + c*svD;
+    [Xu(i,:), DOP(:,:,i)] = gpsPosVel2(rho, X0, svPos, rho_dot, svVel, eye(length(svPrns)));
 end
 
 % Part C: Convert ECEF to LLA and Plot
@@ -31,23 +34,23 @@ figure()
 geoplot(uPosLLA(:,1),uPosLLA(:,2),'.')
 geobasemap satellite
 
-% Part D: Convert ECEF to ENU
-time = time - time(1);
-wgs = wgs84Ellipsoid("meter");
-[uPosE,uPosN,uPosU] = ecef2enu(Xu(:,1), Xu(:,2), Xu(:,3), toomersLLA(1), toomersLLA(2), toomersLLA(3),wgs);
-figure()
-plot(time,uPosE)
-title('East','.')
-figure()
-plot(time,uPosN)
-title('North','.')
-figure()
-plot(time,uPosU)
-title('Up','.')
-
-% Part E: Calculate and find User Velocity in ENU
-% Note: ECEF Velocity states computed in gpsPosVel function
-[uVelE,uVelN,uVelU] = ecef2enu(Xu(:,4),Xu(:,5),Xu(:,6),toomersLLA(1),toomersLLA(2),toomersLLA(3),wgs);
+% % Part D: Convert ECEF to ENU
+% time = time - time(1);
+% wgs = wgs84Ellipsoid("meter");
+% [uPosE,uPosN,uPosU] = ecef2enu(Xu(:,1), Xu(:,2), Xu(:,3), toomersLLA(1), toomersLLA(2), toomersLLA(3),wgs);
+% figure()
+% plot(time,uPosE)
+% title('East','.')
+% figure()
+% plot(time,uPosN)
+% title('North','.')
+% figure()
+% plot(time,uPosU)
+% title('Up','.')
+% 
+% % Part E: Calculate and find User Velocity in ENU
+% % Note: ECEF Velocity states computed in gpsPosVel function
+% [uVelE,uVelN,uVelU] = ecef2enu(Xu(:,4),Xu(:,5),Xu(:,6),toomersLLA(1),toomersLLA(2),toomersLLA(3),wgs);
 
 
 
